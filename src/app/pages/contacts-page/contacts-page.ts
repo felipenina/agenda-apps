ded1e5c98d2a7d530766a58bfad898b2b32edbea
felinina <fnina2645@gmail.com>
2025-10-05 16:40:38 -0300

    cambio de contactspage, en fromulario de N.C

--- src/app/pages/contacts-page/contacts-page.ts
+++ src/app/pages/contacts-page/contacts-page.ts
@@ -1,11 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ContactListItem } from '../../components/contact-list-item/contact-list-item';
-import { NewContact } from '../../interfaces/contact';
+import { Contact, NewContact } from '../../interfaces/contact';
 import { AuthService } from '../../services/auth-service';
 import { contactService } from '../../services/contact.service';
 import { FormsModule } from '@angular/forms';
-
 
 
 @Component({
@@ -14,24 +13,12 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './contacts-page.html',
   styleUrl: './contacts-page.scss'
 })
-export class ContactsPage {
+export class ContactsPage implements OnInit {
+  ngOninit(): void{ 
+    this.contactsService.getContacts();
+  }
 
   authService = inject(AuthService);
   contactsService = inject(contactService);
-contactService: any;
-
-  createContact(form:any){
-    const nuevoContacto: NewContact ={
-      firstName: form.firstName,
-      lastName: form.lastName,
-      email: form.email,
-      address: form.adress,
-      image: form.image,
-      number: form.number,
-      company: form.company,
-      isFavourite: form.isFavorite
-    }
-
-    this.contactsService.createContact(nuevoContacto)
-  }
 }
+
