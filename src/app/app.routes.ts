775c6d13edcb8b5ca5aaa57d9559116d4e6d8833
felinina <fnina2645@gmail.com>
2025-10-15 22:34:12 -0300

    imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { RegisterPage } from './pages/register-page/register-page';
 import { ContactsPage } from './pages/contacts-page/contacts-page';
 import { ContactDetailsPage } from './pages/contact-details-page/contact-details-page';
 import { LoggedLayout } from './layouts/logged-layout/logged-layout';
 import { NewEditContact } from './pages/new-edit-contact/new-edit-contact';
+import { onlyLoggedUserGuard } from './guards/only-logged-user-guard';
+import { onlyPublicUserGuard } from './guards/only-public-user-guard';
+import { GroupsPage } from './pages/groups-page/groups-page';
 
 
 export const routes: Routes = [
@@ -36,7 +40,7 @@ export const routes: Routes = [
       {
         path: "contacts/:idContacto/edit",
         component: NewEditContact
-      }
+      },
       {
         path: "groups",
         component: GroupsPage
