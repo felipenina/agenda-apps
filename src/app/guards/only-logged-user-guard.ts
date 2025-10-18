c764e7e2fd624b41921b2c67d036c7ace51b3264
felinina <fnina2645@gmail.com>
2025-10-18 08:48:40 -0300

    completado de los guards

--- src/app/guards/only-logged-user-guard.ts
+++ src/app/guards/only-logged-user-guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateChildFn, RedirectCommand, Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
-export const onlyLoggedUserGuard: CanActivateFn = (route, state) => {
+export const onlyLoggedUserGuard: CanActivateChildFn = (childRoute, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  if(!auth.token){
+    const newPath = router.parseUrl("/login");
+    return new RedirectCommand(newPath, {
+      skipLocationChange: true,
+    });
+  }
   return true;
 };
--- src/app/guards/only-public-user-guard.ts
+++ src/app/guards/only-public-user-guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, RedirectCommand, Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
 export const onlyPublicUserGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  if(auth.token){
+    const newPath = router.parseUrl("/");
+    return new RedirectCommand(newPath, {
+      skipLocationChange: true,
+    });
+  }
   return true;
 };
